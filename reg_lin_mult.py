d80b97e0d56103a9c22335cfab11ae043c359834
if09325 <51119971+if09325@users.noreply.github.com>
2020-04-21 11:49:56 +0200

    Update reg_lin_mult.py

--- reg_lin_mult.py
+++ reg_lin_mult.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd 
+#pour le graph
 import matplotlib.pyplot as plt 
+#pour le résultat détaillé sur les différentes valeur R² - P-Value ...etc
 from statsmodels.regression.linear_model import OLS as ols
 
-#loading data 
+#chargement des données 
 df = pd.read_csv("ds.csv")
 
-#creation des Xs, Y 
+#creation des variables (dép | indép)
 Y_profit = df["Profit"] 
-
 X_indepent_var = df[["R&D Spend", "Administration", "Marketing Spend"]]
 #X_indepent_var = df[["R&D Spend", "Marketing Spend"]]
 #X_indepent_var = df[["R&D Spend", "Administration"]]
 
+#Dessiner le graphe en 3D
 ax = plt.axes(projection='3d')
-
 ax.set_xlabel("R&D Spend")
 ax.set_ylabel("Marketing Spend")
 ax.set_zlabel("Profit ")
@@ -23,9 +24,8 @@ z_points = df["R&D Spend"]
 x_points = df["Administration"]
 y_points = df["Profit"]
 ax.scatter3D(x_points, y_points, z_points, c=z_points);
-
 plt.show()
 
+#Détailler les résultats statistiques 
 res = ols(Y_profit,X_indepent_var).fit()
-
 print(res.summary())
